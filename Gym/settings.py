c152bf711fcd32ad5594a79af5bb383eff34a5db
Tomasz <tomek_kasperczyk@interia.pl>
2021-04-27 11:18:14 +0200

    fic static

--- Gym/settings.py
+++ Gym/settings.py
@@ -151,14 +151,14 @@ REST_FRAMEWORK = {
 }
 
 # Static files (CSS, JavaScript, Images)
-STATIC_ROOT = os.path.join(BASE_DIR, "/static/")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    BASE_DIR, "static"
+    BASE_DIR,
 ]
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
